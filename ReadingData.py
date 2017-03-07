9a1cfac515b083d1a56feda270e79814a166bf4d
ism0080 <isaac.mackle@gmail.com>
2017-03-07 17:51:38 +1300

    Can sorta read from file Added some more regex cases

--- ReadingData.py
+++ ReadingData.py
@@ -1,22 +1,45 @@
 import re
 
-ID = "A123"
-GENDER = "M"
-AGE = 12
-SALES = 123
-BMI = "NORMAL"
-SALARY = 10.999
-BIRTHDAY = "24-10-1996"
+# ID = "A123"
+# GENDER = "M"
+# AGE = 12
+# SALES = 123
+# BMI = "NORMAL"
+# SALARY = 10.999
+# BIRTHDAY = "24-10-1996"
 
+# file = open("table.txt", "r")
+# for line in file:
+#     print(line)
+
+with open('table.txt', 'r') as file:
+    data = file.read()
+    print(data)
 
 class Validator(object):
 
     def empid(self, empid):
-        match = re.match(r'[A-Z][1-9]{3}', empid)
+        match = re.search(r'[A-Z][1-9]{3}', empid)
         if match is not None:
             clean = match.group(0)
         else:
-            clean = "Not a Valid ID"
+            clean = ""
+        return clean
+
+    def gender(self, gender):
+        match = re.search(r'M|F', gender)
+        if match is not None:
+            clean = match.group(0)
+        else:
+            clean = ""
+        return clean
+
+    def age(self, age):
+        match = re.search(r'[0-9]{2}', age)
+        if match is not None:
+            clean = match.group(0)
+        else:
+            clean = ""
         return clean
 
 class Table(object):
@@ -33,10 +56,13 @@ class Table(object):
     def say(self, msg):
         return "EMPID={id}, GENDER={gender}, AGE={age}, SALES={sales}, BMI={bmi}, SALARY={salary}, BIRTHDAY={birthday}".format(id=self.id, gender=self.gender, age=self.age, sales=self.sales, bmi=self.bmi, salary=self.salary, birthday=self.birthday )
 
+
 e = Validator()
-print(e.empid(ID))
-j = Table(e.empid(ID), GENDER, AGE, SALES, BMI, SALARY, BIRTHDAY)
-print(j.say("mes"))
+print(e.empid(data))
+print(e.gender(data))
+print(e.age(data))
+# j = Table(e.empid(ID), GENDER, AGE, SALES, BMI, SALARY, BIRTHDAY)
+# print(j.say("mes"))
 
 # try:
 #     matchObj = re.match(r'[A-Z][1-9]{3}', ID)
