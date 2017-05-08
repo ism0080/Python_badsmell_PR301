a77d9623c2c4c646a9868b3fbfe592da9ba9a320
ism0080 <isaac.mackle@gmail.com>
2017-05-09 09:46:19 +1200

    Refactoring 2: Comment Smell - def read() - line 5 Extract a method called ‘create_list_of_dictionaries’

--- reader.py
+++ reader.py
@@ -13,13 +13,16 @@ class FileReader(object):
                 # splits on comma separation
                 self.splits_on_comma_separation(new_line_split)
                 # adds each dictionary to a dictionary to create a list of dictionaries
-                for empty in self.list_of_dictionaries:
-                    self.list_of_dictionaries[empty]\
-                        = dict(e.split('=') for e in self.line[empty])
+                self.create_list_of_dictionaries()
             return self.list_of_dictionaries
         except IOError as err:
             print("The exception is: ", err)
             pass
+
+    def create_list_of_dictionaries(self):
+        for empty in self.list_of_dictionaries:
+            self.list_of_dictionaries[empty] \
+                = dict(e.split('=') for e in self.line[empty])
 
     def splits_on_comma_separation(self, new_line_split):
         i = 0
